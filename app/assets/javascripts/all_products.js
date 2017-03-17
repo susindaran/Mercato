52b70f09feb3007d066f67676bd4c1e282bc6425
Susindaran Elangovan <susindaran94@gmail.com>
2017-03-17 10:39:13 -0500

    Added modal for product delete icon

--- app/assets/javascripts/all_products.js
+++ app/assets/javascripts/all_products.js
@@ -14,23 +14,36 @@ MERCATO.Admin.AllProducts = {
     {
         MERCATO.Admin.AllProducts.pageSize = parseInt( element.value );
         MERCATO.Admin.AllProducts.calculatePages();
-
+        MERCATO.Admin.AllProducts.redrawNavigationBar();
+    },
+    redrawNavigationBar: function()
+    {
         var navigationBar = $('#ulPageNavigation');
         navigationBar.twbsPagination('destroy');
         navigationBar.twbsPagination(
-        {
-            totalPages: MERCATO.Admin.AllProducts.pages,
-            visiblePages: 7,
-            onPageClick: function (event, page)
             {
-                MERCATO.Admin.AllProducts.getProductTable( page );
-            }
-        });
-
+                totalPages: MERCATO.Admin.AllProducts.pages,
+                visiblePages: 7,
+                onPageClick: function (event, page)
+                {
+                    MERCATO.Admin.AllProducts.getProductTable( page );
+                }
+            });
     },
     calculatePages: function()
     {
         MERCATO.Admin.AllProducts.pages = (MERCATO.Admin.AllProducts.totalCount / MERCATO.Admin.AllProducts.pageSize) +
             (MERCATO.Admin.AllProducts.totalCount % MERCATO.Admin.AllProducts.pageSize == 0 ? 0 : 1);
+    },
+    openDeleteModal: function( element )
+    {
+        var elementId = element.parentNode.id;
+        console.log(elementId);
+        var rowIndex = elementId.split("-")[1];
+        var modalContent = $("#divDeleteProductModalContent");
+        var productID = $("#tdProductId-"+rowIndex).text();
+        modalContent.text("Are you sure you want to delete the product - " + productID);
+
+        $("#divDeleteProductModal").modal('open');
     }
 };
